8c38c2c057394d30077db8d2be7a933dd7bf8cd4
HumanJBooF <humanjboof@gmail.com>
2018-07-29 20:26:50 -0400

    remove button and sign in button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,14 +6,12 @@ $(function () {
         authDomain: "train-database-eadf6.firebaseapp.com",
         databaseURL: "https://train-database-eadf6.firebaseio.com",
         projectId: "train-database-eadf6",
-        storageBucket: "",
+        storageBucket: "train-database-eadf6.appspot.com",
         messagingSenderId: "430166995585"
     };
-
     firebase.initializeApp(config);
 
     var database = firebase.database();
-
 
     var pushToBase = () => {
         //creating objext to send to database
@@ -24,32 +22,25 @@ $(function () {
             frequency: $('.frequency').val().trim(),
             dateAdded: firebase.database.ServerValue.TIMESTAMP
         };
+
         //sending to database
         database.ref('new_train').push(newTrain);
         //gotta console log you know
-        console.log('Checking new object', newTrain);
-        console.log('this is button');
+        // console.log('Checking new object', newTrain);
+        // console.log('this is button');
     };
 
     //reseting input fields(I know the name didn't give that a way)
     var resetFields = () => {
         $('.train, .destination, .time, .frequency').val("");
     };
+
     //display live time
     function displayTime() {
-        var time = moment().format('HH:mm:ss');
+        var time = moment().format('hh:mm:ss A');
         $('.nav-title').html(time);
         setTimeout(displayTime, 1000);
     }
-
-
-
-    //click me function and it like tottally calls some things you know
-    $('.submit').on('click', function () {
-        event.preventDefault();
-        pushToBase();
-        resetFields();
-    });
 
     database.ref('new_train').on('child_added', function (childSnapshot) {
         console.log('This is CHILD ADDED', childSnapshot.val());
@@ -61,7 +52,7 @@ $(function () {
 
         var newFreq = parseInt(freq)
         // first train time going back in time like Marty McFly to come before the current time
-        var firstTimeConverted = moment(firstTrainTime, 'HH:mm').subtract(1, 'years');
+        var firstTimeConverted = moment(firstTrainTime, 'hh:mm').subtract(1, 'years');
         // Difference between times (omg no way!)
         var timeBetween = moment().diff(moment(firstTimeConverted), 'minutes')
         // Dividing the remaining time with the frequency 
@@ -70,22 +61,62 @@ $(function () {
         var minutesTillTrain = newFreq - timeRemainder;
         // Next trains coming down the track choooo choooooo
         var nextTrain = moment().add(minutesTillTrain, 'minutes')
-        var displayNext = moment(nextTrain).format('HH:mm A')
+        var displayNext = moment(nextTrain).format('hh:mm A')
 
-        // var removeButton = $('<button>').addClass(`waves-effect waves-light btn`).text('remove')
-        // <td>${removeButton}</td>
-        $('.body').append(
-            `<tr><td>${trainName}</td>
+
+        $('.tBody').append(
+            `<tr>
+              <td>${trainName}</td>
               <td>${destination}</td>
               <td>${freq}</td>
               <td>${displayNext}</td>
               <td>${minutesTillTrain}</td>
-              </tr>`
+              <td><a class="remove waves-effect waves-light btn-small"><i class="material-icons">remove</i></a></td>
+             </tr>`
         );
 
     });
+
+    //click me function and it like tottally calls some things you know
+    $('.submit').on('click', function () {
+        event.preventDefault();
+        pushToBase();
+        resetFields();
+    });
+
+
+    $(document).on('click', '.remove', function () {
+        $(this).closest('tr').remove();
+        // To do, remove from database on click
+    })
+
+    //Almost working.... Sign in with google click event
+    $('.signin').on('click', function () {
+        console.log('click')
+        var provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithPopup(provider).then(function (result) {
+            var token = result.credential.accessToken;
+            var user = result.user;
+
+            console.log(token)
+            console.log(user)
+        }).catch(function (error) {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+
+            console.log(error.code)
+            console.log(error.message)
+        });
+
+    })
+    //reloads page every minute to display accurate train times ... trying to find a better way to do this
+    setInterval(function () {
+        location.reload();
+    }, 60000)
+    
     //current Time
     displayTime()
     $('.parallax').parallax();
+
 
 });

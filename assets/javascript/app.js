abd866061895c43e7662669ac46ffa4106b6e6cc
HumanJBooF <humanjboof@gmail.com>
2018-08-11 19:50:30 -0400

    es6 and spell check

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,7 @@ $(function() {
 
     $('.card').hide()
     // Initialize Firebase
-    var config = {
+    const config = {
         apiKey: "AIzaSyD3Ax76YqmjJii-sZcTpolidVB-VleH58k",
         authDomain: "train-database-eadf6.firebaseapp.com",
         databaseURL: "https://train-database-eadf6.firebaseio.com",
@@ -12,15 +12,15 @@ $(function() {
     };
     firebase.initializeApp(config);
 
-    var database = firebase.database();
+    const database = firebase.database();
 
-    var pushToBase = () => {
-        var trainName = $('.train').val().trim();
-        var destination = $('.destination').val().trim();
-        var firstTrain = $('.time').val().trim();
-        var freq = $('.frequency').val().trim();
+    const pushToBase = () => {
+        let trainName = $('.train').val().trim();
+        let destination = $('.destination').val().trim();
+        let firstTrain = $('.time').val().trim();
+        let freq = $('.frequency').val().trim();
         //creating objext to send to database
-        var newTrain = {
+        const newTrain = {
             name: trainName,
             destination: destination,
             time: firstTrain,
@@ -28,7 +28,7 @@ $(function() {
             dateAdded: firebase.database.ServerValue.TIMESTAMP
         }
         //checking to make sure all form fields and filled
-        if (trainName === "" || destination === "" || firstTrain === "" || freq === "") {
+        if (!trainName || !destination || !firstTrain || !freq ) {
             $( ".card" ).fadeIn("fast");
 
             setTimeout(function(){ 
@@ -46,14 +46,14 @@ $(function() {
      
     };
 
-    //reseting input fields(I know the name didn't give that a way)
-    var resetFields = () => {
+    //resetting input fields(I know the name didn't give that a way)
+    const resetFields = () => {
         $('.train, .destination, .time, .frequency').val("");
     };
 
     //display live time
-    var displayTime = () => {
-        var time = moment().format('hh:mm:ss A');
+    const displayTime = () => {
+        let time = moment().format('hh:mm:ss A');
         $('.nav-title').html(time);
         setTimeout(displayTime, 1000);
     }
@@ -61,24 +61,24 @@ $(function() {
     database.ref('new_train').on('child_added', function (childSnapshot) {
         console.log('This is CHILD ADDED', childSnapshot.val());
 
-        var trainName = childSnapshot.val().name;
-        var destination = childSnapshot.val().destination;
-        var freq = childSnapshot.val().frequency
-        var firstTrainTime = childSnapshot.val().time
-        var key = childSnapshot.key
+        let trainName = childSnapshot.val().name;
+        let destination = childSnapshot.val().destination;
+        let freq = childSnapshot.val().frequency
+        let firstTrainTime = childSnapshot.val().time
+        let key = childSnapshot.key
 
-        var newFreq = parseInt(freq)
+        let newFreq = parseInt(freq)
         // first train time going back in time like Marty McFly to come before the current time
-        var firstTimeConverted = moment(firstTrainTime, 'hh:mm').subtract(1, 'years');
+        let firstTimeConverted = moment(firstTrainTime, 'hh:mm').subtract(1, 'years');
         // Difference between times (omg no way!)
-        var timeBetween = moment().diff(moment(firstTimeConverted), 'minutes')
+        let timeBetween = moment().diff(moment(firstTimeConverted), 'minutes')
         // Dividing the remaining time with the frequency 
-        var timeRemainder = timeBetween % newFreq;
+        let timeRemainder = timeBetween % newFreq;
         // well well well now here we got to subtract the frequency from the remaining till to get the minutes until next train you know?!
-        var minutesTillTrain = newFreq - timeRemainder;
+        let minutesTillTrain = newFreq - timeRemainder;
         // Next trains coming down the track choooo choooooo
-        var nextTrain = moment().add(minutesTillTrain, 'minutes')
-        var displayNext = moment(nextTrain).format('hh:mm A')
+        let nextTrain = moment().add(minutesTillTrain, 'minutes')
+        let displayNext = moment(nextTrain).format('hh:mm A')
 
 
         $('.tBody').append(
@@ -96,7 +96,7 @@ $(function() {
         console.log(`Uh I think you have yourself an error boy ${error}`);
     });
 
-    //click me function and it like tottally calls some things you know
+    //click me function and it like totally calls some things you know
     $('.submit').on('click', function () {
         event.preventDefault();
         pushToBase();
@@ -105,7 +105,7 @@ $(function() {
 
 
     $(document).on('click', '.remove', function () {
-        var deleteKey = $(this).attr("id");
+        let deleteKey = $(this).attr("id");
         database.ref('new_train').child(deleteKey).remove();
 
         location.reload();
@@ -114,17 +114,17 @@ $(function() {
     //Almost working.... Sign in with google click event
     $('.signin').on('click', function () {
         console.log('click')
-        var provider = new firebase.auth.GoogleAuthProvider();
+        let provider = new firebase.auth.GoogleAuthProvider();
         firebase.auth().signInWithPopup(provider).then(function (result) {
-            var token = result.credential.accessToken;
-            var user = result.user;
+            let token = result.credential.accessToken;
+            let user = result.user;
         
                 console.log(token)
                 console.log(user)
 
         }).catch(function (error) {
-            var errorCode = error.code;
-            var errorMessage = error.message;
+            let errorCode = error.code;
+            let errorMessage = error.message;
 
             console.log(error.code)
             console.log(error.message)
